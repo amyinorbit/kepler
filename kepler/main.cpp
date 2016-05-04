3272fdd4bbc195d0ccaa10f6cc33744d449d1157
Amy Parent <amy@amyparent.com>
2016-05-04 14:22:30 +0100

    Initial Commit

---
+++ kepler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kepler
+//
+//  Created by Cesar Parent on 04/05/2016.
+//  Copyright © 2016 cesarparent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
